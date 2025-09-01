cb675d9af6d10436857feb586e2ef48d3febefcd
Santiagoo-2 <santiagojm2005@gmail.com>
2025-09-01 10:54:32 -0600

    Add files via upload

---
+++ ejemplosize1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nSize of 'int' primitive data type: %d", sizeof(int));   
+	printf("\nSize of 'float' primitive data type: %d", sizeof(float));
+	printf("\nSize of 'double' primitive data type: %d", sizeof(double));
+	printf("\nSize of 'char' primitive data type: %d", sizeof(char));
+//sizeof sire para que te devuelva el valor de cuantos bytes que necesitamos para representar cada dato
+
+    return 0;
+}
